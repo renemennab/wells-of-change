4ff28f74c18367b3906108c28afc1edede167f51
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2021-11-22 19:54:01 -0300

    start homepage

---
+++ src/home/homePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export function HomePage(): JSX.Element {
+    return (
+        <HomePageStyles>
+            <span>helloooooooo</span>
+        </HomePageStyles>
+    )
+}
+
+const HomePageStyles = styled.div`
+ 
+`
