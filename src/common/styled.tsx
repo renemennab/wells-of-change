603099223c006b67c1340f0b8b1b2bf87c2c541b
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2021-11-22 19:54:30 -0300

    add styled heading 1

---
+++ src/common/styled.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const styledH1 = styled.h1`
+    color: var(--dark-blue);
+    font-size: 25px;
+    text-transform: capitalize;
+    font-family: 'Josefin Sans', sans-serif;
+`
